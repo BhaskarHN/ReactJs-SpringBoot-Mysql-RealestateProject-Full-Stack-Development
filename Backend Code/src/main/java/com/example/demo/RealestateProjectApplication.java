61a71e0aff71929c86759ea34e44ef9dd9c23425
BhaskarHN18 <baskarhn06@gmail.com>
2024-05-29 15:10:32 +0530

    Create RealestateProjectApplication.java

---
+++ Backend Code/src/main/java/com/example/demo/RealestateProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class RealestateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateProjectApplication.class, args);
+	}
+
+}
